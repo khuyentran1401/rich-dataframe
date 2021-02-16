41f30115acaf7101e3866dcf52e38e4e48525f83
khuyentran1401 <khuyentran1476@gmail.com>
2021-02-15 22:05:46 -0600

    add support pandas Series

--- example.py
+++ example.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from sklearn.datasets import fetch_openml
 
 from rich_dataframe import prettify
@@ -5,7 +6,9 @@ from rich_dataframe import prettify
 if __name__ == "__main__":
 
     speed_dating = fetch_openml(name="SpeedDating", version=1)["frame"]
-
+    speed_dating = speed_dating.iloc[:, 0]
+    print(isinstance(speed_dating, pd.Series))
+    print(speed_dating.name)
     table = prettify(
         speed_dating, row_limit=20, first_rows=False, delay_time=5
     )
