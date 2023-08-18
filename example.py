70c99dc6c9381acd19e535cf4993d4094f8068d5
Sebastian Seck <sebas.seck@gmail.com>
2023-08-18 17:37:17 +0200

    bump up versions

--- example.py
+++ example.py
@@ -3,8 +3,9 @@ from sklearn.datasets import fetch_openml
 from rich_dataframe import prettify
 
 if __name__ == "__main__":
-
-    speed_dating = fetch_openml(name="SpeedDating", version=1)["frame"]
+    speed_dating = fetch_openml(name="SpeedDating", version=1, parser="auto")[
+        "frame"
+    ]
     table = prettify(
         speed_dating,
         row_limit=20,
